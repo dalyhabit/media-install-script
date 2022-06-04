4c9ca4ac22725f937bd53d502cad42a826aa2b47
Mark <mediabanana@mclayton.net>
2022-06-04 19:56:21 +0000

    Update filebrowser.sh

--- modules/generators/filebrowser.sh
+++ modules/generators/filebrowser.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 echo -e """  filebrowser:
-    image: hurlenko/filebrowser
+    image: filebrowser/filebrowser
     container_name: filebrowser
     user: "1000:1000"
     ports:
-      - 8080:8080
+      - 8080:80
     volumes:
       - ${storage}:/srv
-      - ${config}/filebrowser:/config
-    environment:
-      - FB_BASEURL=/filebrowser
+      - ${config}/filebrowser/settings.json:/.filebrowser.json
+      - ${config}/filebrowser/db:/database
     restart: always"""
