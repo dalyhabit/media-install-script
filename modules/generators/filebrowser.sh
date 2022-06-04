c2a224383f2a93f5007beb8759a5191f869ce08d
Mark <mediabanana@mclayton.net>
2022-06-04 19:35:51 +0000

    Update filebrowser.sh

--- modules/generators/filebrowser.sh
+++ modules/generators/filebrowser.sh
@@ -4,7 +4,7 @@ echo -e """  filebrowser:
     container_name: filebrowser
     user: "1000:1000"
     ports:
-      - 443:8080
+      - 8080:8080
     volumes:
       - ${storage}:/storage
       - ${config}/filebrowser:/config
